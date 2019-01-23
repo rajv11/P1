957b970aed69499a5b094af55a3dff78bcd9d463
vamsi <vamsi.raj11@gmail.com>
2019-01-22 18:25:19 -0600

    Reading customer name and welcomimg them.

--- P1/Program.cs
+++ P1/Program.cs
@@ -6,9 +6,9 @@ namespace P1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to restraunt! \nPlease provide your name: ");
+            Console.WriteLine("Please provide your name: ");
             var name = Console.ReadLine();
-
+            Console.WriteLine($"Welcome to restraunt {name} !!");
             
 
         }
